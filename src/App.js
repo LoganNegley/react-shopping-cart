22694b6e0732e746b9abf6f768b8dc8af75f5534
Logan Negley <logannegley@gmail.com>
2020-03-05 19:47:43 -0600

    remove item from cart function made

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ function App() {
 		setCart([...cart, item])
 	};
 
+	const removeItem = (id) => {
+		setCart(cart.filter(item => item.id !== id ))
+	}
+
 	return (
 		<div className="App">
 		<ProductContext.Provider value={{products, addItem}}>
