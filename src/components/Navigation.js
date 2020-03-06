a190dfb13bb1fd591eca738a8f728b262123b252
Logan Negley <logannegley@gmail.com>
2020-03-05 19:33:49 -0600

    ShoopingCart rerendered using useContext

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {useContext} from 'react';
+import CartContext from '../contexts/CartContext';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = props => {
